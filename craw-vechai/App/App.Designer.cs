85f992ac03c6acbaf967f6ca1da6287eb641b5b6
Phuong Tran <phuong.tran@saigontechnology.vn>
2016-03-17 15:33:42 +0700

    Init project

---
+++ craw-vechai/App/App.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace App
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(48, 22);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(391, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrl.Location = new System.Drawing.Point(12, 25);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(23, 13);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "Url";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(120, 50);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnCraw
+            // 
+            this.btnCraw.Location = new System.Drawing.Point(271, 50);
+            this.btnCraw.Name = "btnCraw";
+            this.btnCraw.Size = new System.Drawing.Size(75, 23);
+            this.btnCraw.TabIndex = 3;
+            this.btnCraw.Text = "Craw";
+            this.btnCraw.UseVisualStyleBackColor = true;
+            this.btnCraw.Click += new System.EventHandler(this.btnCraw_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 82);
+            this.Controls.Add(this.btnCraw);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "App";
+            this.Text = "App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCraw;
+    }
+}
---
+++ craw-vechai/App/App.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+            txtUrl.Text = @"http://vechai.info/one-piece";
+        }
+
+        private void btnCraw_Click(object sender, System.EventArgs e)
+        {
+            var url = txtUrl.Text;
+
+            var pageContent = new WebClient { Encoding = Encoding.UTF8 }.DownloadString(url);
+
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument { OptionDefaultStreamEncoding = Encoding.UTF8 };
+            htmlDoc.LoadHtml(pageContent);
+
+            var parentChapterListNote = htmlDoc.DocumentNode.Descendants().FirstOrDefault(x => x.Id == "chapterList");
+            if (parentChapterListNote == null)
+            {
+                return;
+            }
+
+            var chapterListNote = parentChapterListNote.Descendants().LastOrDefault(x => x.Attributes.Contains("class") && x.Attributes["class"].Value.Contains("inner"));
+            if (chapterListNote == null)
+            {
+                return;
+            }
+
+
+            var chapters = chapterListNote.Descendants("a").ToList();
+            foreach (var chapter in chapters)
+            {
+                var href = chapter.Attributes["href"] != null ? chapter.Attributes["href"].Value : "";
+                var title = chapter.InnerText;
+
+                CrawChapter(href);
+            }
+        }
+
+        private void CrawChapter(string url)
+        {
+            var pageContent = new WebClient { Encoding = Encoding.UTF8 }.DownloadString(url);
+
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument { OptionDefaultStreamEncoding = Encoding.UTF8 };
+            htmlDoc.LoadHtml(pageContent);
+
+            var cc = htmlDoc.DocumentNode.InnerHtml;
+        }
+    }
+}
