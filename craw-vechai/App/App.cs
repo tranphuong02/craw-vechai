ba000194a64276f3ed6f2fc042212b9fd47ef4a9
Phuong Tran <phuong.tran@saigontechnology.vn>
2016-03-24 16:20:19 +0700

    Download img files

--- craw-vechai/App/App.cs
+++ craw-vechai/App/App.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
@@ -41,18 +42,67 @@ namespace App
                 var href = chapter.Attributes["href"] != null ? chapter.Attributes["href"].Value : "";
                 var title = chapter.InnerText;
 
-                CrawChapter(href);
+                CrawChapter(href, title);
             }
         }
 
-        private void CrawChapter(string url)
+        private void CrawChapter(string url, string title)
         {
             var pageContent = new WebClient { Encoding = Encoding.UTF8 }.DownloadString(url);
 
             var htmlDoc = new HtmlAgilityPack.HtmlDocument { OptionDefaultStreamEncoding = Encoding.UTF8 };
             htmlDoc.LoadHtml(pageContent);
 
-            var cc = htmlDoc.DocumentNode.InnerHtml;
+            RemoveNoNeedHtml(htmlDoc);
+
+            var contentNote = htmlDoc.DocumentNode.Descendants().FirstOrDefault(x => x.Attributes["id"] != null && x.Attributes["id"].Value == "contentChapter");
+            if (contentNote == null)
+            {
+                return;
+            }
+            var imgs = contentNote.Descendants().Where(x => x.Name == "img").ToList();
+            if (imgs.Any() == false)
+            {
+                return;
+            }
+
+            foreach (var imgNote in imgs)
+            {
+                var srcAttr = imgNote.Attributes["src"];
+                var altAttr = imgNote.Attributes["alt"];
+
+                if (srcAttr == null)
+                {
+                    continue;
+                }
+
+                using (var webClient = new WebClient())
+                {
+                    try
+                    {
+                        var uri = new Uri(srcAttr.Value);
+                        var query = uri.Query + "";
+                        var path = uri.AbsolutePath.Replace(query, "");
+                        var subStringIndex = path.LastIndexOf(@"/") + 1;
+                        var fileName = path.Substring(subStringIndex, path.Length - subStringIndex);
+
+                        if (fileName.Contains("."))
+                        {
+                            webClient.DownloadFile(srcAttr.Value, fileName);
+                        }
+                    }
+                    catch (Exception)
+                    { }
+                }
+            }
+        }
+
+        private void RemoveNoNeedHtml(HtmlAgilityPack.HtmlDocument htmlDoc)
+        {
+            htmlDoc.DocumentNode.Descendants()
+               .Where(n => n.Name == "script" || n.Name == "style" || n.Name == "noscript")
+               .ToList()
+               .ForEach(n => n.Remove());
         }
     }
 }
